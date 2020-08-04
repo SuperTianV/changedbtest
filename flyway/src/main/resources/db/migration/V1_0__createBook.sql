06f131a498200d3ff5b9299e95c2ce92066a96c8
admin <admin@example.com>
2020-08-04 18:37:38 +0800

    test changedb init

---
+++ flyway/src/main/resources/db/migration/V1_0__createBook.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `book`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `create_time` datetime(0) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+INSERT INTO book(name, create_time) VALUES ('测试1', '2020-04-21 16:53:48');
---
+++ flyway/src/main/resources/db/migration/V1_2__insertBook.sql
@@ -0,0 +1,1 @@
+INSERT INTO book(name, create_time) VALUES ('测试3', '2020-04-21 16:53:48');
